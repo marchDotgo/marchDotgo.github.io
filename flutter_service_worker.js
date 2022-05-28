2551c33dffca55ed9a94d3f2cc86ea6f6de6547a
Ananthuls <102660926+Ananthuls@users.noreply.github.com>
2022-05-28 23:11:40 +0530

    ananthu

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -50,9 +50,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "32debdb609ee206e4e9634df902e281d",
-"/": "32debdb609ee206e4e9634df902e281d",
-"main.dart.js": "2dab2136c89b282de466585fbcac9fa5",
+"index.html": "3fca09aed4c41ef1b1a6698112e3b791",
+"/": "3fca09aed4c41ef1b1a6698112e3b791",
+"main.dart.js": "1626f0ac5f19e0535a9afbafd5ca7a2f",
 "manifest.json": "96db185e8a291dcd58846ba3d30375fa",
 "version.json": "227cfeb6ef816299e8de0aefb91214d2"
 };
