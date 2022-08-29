e4cf0f9b0a22e39b4012634bfcaaadccce704fd8
ashimMarch <104412075+ashimMarch@users.noreply.github.com>
2022-08-29 22:41:47 +0530

    New version 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,9 +51,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f789cf652072a38c58e34846c8a8a414",
-"/": "f789cf652072a38c58e34846c8a8a414",
-"main.dart.js": "53814cf95385db1f2d7be35c0429f61c",
+"index.html": "981768939bec0bf075bd06fb5b531eb9",
+"/": "981768939bec0bf075bd06fb5b531eb9",
+"main.dart.js": "3b8fe2e8abb5d12f77e7d347ab6346a6",
 "manifest.json": "5626dfe1d1c1c9352a83684a52a821a7",
 "version.json": "227cfeb6ef816299e8de0aefb91214d2"
 };
