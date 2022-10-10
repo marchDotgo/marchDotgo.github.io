30517f11a98b4cd20062cdc0780243514e5ac86c
Ananthuls <102660926+Ananthuls@users.noreply.github.com>
2022-10-10 16:17:34 +0530

    a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "897d9927ffe22f001a1d0b10bdaf7a44",
-"index.html": "5ec5f7d73c575bf8361bdaa4a70ea5b6",
-"/": "5ec5f7d73c575bf8361bdaa4a70ea5b6",
-"main.dart.js": "7ddb110d5437f8aeb81c80efcedff168",
+"index.html": "2489aadedd48a84e0ace5291e054ffc0",
+"/": "2489aadedd48a84e0ace5291e054ffc0",
+"main.dart.js": "d3ae39dea297e658139f06e5c8bd2c59",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "b9ed2d2188b86ecbf26cc1edbaf1dba8",
 "icons/Icon-192.png": "ead59ab3b9d35d922a685909672be504",
